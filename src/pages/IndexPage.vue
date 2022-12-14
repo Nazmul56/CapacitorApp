57b5911f1a37a82ee4820b56135301165879b7d9
Nazmul Haque <nazmul.cste07@gmail.com>
2022-12-14 18:13:24 +0600

    Update IndexPage.vue
    delete unused code

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -182,28 +182,6 @@ export default {
       const imageUrl = image.webPath
 
       console.log('Image Url: ' + imageUrl)
-      // Can be set to the src of an image now
-      // imageElement.src = imageUrl
-
-      /* const takePicture = async () => {
-        console.log('Inside Tsking pic')
-        const image = await Camera.getPhoto({
-          quality: 90,
-          allowEditing: true,
-          resultType: CameraResultType.Uri
-        })
-
-        // image.webPath will contain a path that can be set as an image src.
-        // You can access the original file using image.path, which can be
-        // passed to the Filesystem API to read the raw data of the image,
-        // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
-        const imageUrl = image.webPath
-
-        console.log('Image Url: ' + imageUrl)
-        // Can be set to the src of an image now
-        // imageElement.src = imageUrl
-      }
-      console.log('Image taker": ' + takePicture) */
     }
   }
 }
